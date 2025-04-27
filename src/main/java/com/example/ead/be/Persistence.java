95c1cd7ffcceca5fae1fbe212841b4ee63f18ef1
venkat Akurathi <venkat.akurathi@gmail.com>
2025-04-27 21:30:42 +0100

    fix-deployment

--- src/main/java/com/example/ead/be/Persistence.java
+++ src/main/java/com/example/ead/be/Persistence.java
@@ -24,6 +24,7 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @Component
 public class Persistence {
+    private static final Logger LOGGER = Logger.getLogger(Persistence.class.getName());
     public static List<Recipe> recipes = Arrays.asList(
             new Recipe("elotes",
                     Arrays.asList("corn", "mayonnaise", "cotija cheese", "sour cream", "lime"),
@@ -44,13 +45,27 @@ public class Persistence {
     private MongoCollection<Recipe> collection = null;
 
     public Persistence() {
-        String connString = System.getenv().getOrDefault("MONGO_URI", "mongodb://admin:securepassword@localhost:27017/ead_2024?authSource=admin");
-        String dbName = System.getenv().getOrDefault("MONGO_DATABASE", "ead_2024");
+        String connString = System.getenv("MONGO_URI");
+        String dbName = System.getenv("MONGO_DATABASE");
         String collectionName = "ead_2024";
+
+        if (connString == null || connString.isEmpty()) {
+            throw new IllegalStateException("MONGO_URI environment variable is not set");
+        }
+        if (dbName == null || dbName.isEmpty()) {
+            throw new IllegalStateException("MONGO_DATABASE environment variable is not set");
+        }
+
         initMongoDBClient(connString, dbName, collectionName);
     }
 
     public Persistence(String connString, String dbName, String colName) {
+        if (connString == null || connString.isEmpty()) {
+            throw new IllegalStateException("MongoDB connection string is required");
+        }
+        if (dbName == null || dbName.isEmpty()) {
+            throw new IllegalStateException("MongoDB database name is required");
+        }
         initMongoDBClient(connString, dbName, colName);
     }
 
@@ -62,37 +77,58 @@ public class Persistence {
                     fromProviders(PojoCodecProvider.builder().automatic(true).build()));
             MongoClientSettings settings = MongoClientSettings.builder()
                     .codecRegistry(pojoCodecRegistry)
-                    .applyConnectionString(mongoUri).build();
-            try {
-                mongoClient = MongoClients.create(settings);
-                System.out.println("Connected to MongoDB: " + connString);
-            } catch (MongoException me) {
-                System.err.println("Unable to connect to MongoDB: " + me);
-                throw new RuntimeException("MongoDB connection failed", me);
+                    .applyConnectionString(mongoUri)
+                    .retryWrites(true)
+                    .retryReads(true)
+                    .build();
+            int retries = 5;
+            while (retries > 0) {
+                try {
+                    mongoClient = MongoClients.create(settings);
+                    database = mongoClient.getDatabase(dbName);
+                    collection = database.getCollection(collectionName, Recipe.class);
+                    // Test connection
+                    collection.countDocuments();
+                    LOGGER.info("Connected to MongoDB: " + connString + ", database: " + dbName + ", collection: " + collectionName);
+                    break;
+                } catch (MongoException me) {
+                    LOGGER.severe("MongoDB connection attempt failed: " + me.getMessage());
+                    retries--;
+                    if (retries == 0) {
+                        throw new RuntimeException("MongoDB connection failed after retries", me);
+                    }
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
             }
         }
-        database = mongoClient.getDatabase(dbName);
-        collection = database.getCollection(collectionName, Recipe.class);
-        System.out.println("Initialized database: " + dbName + ", collection: " + collectionName);
     }
 
     public List<Recipe> getAllRecipes() {
-        MongoCursor<Recipe> cur = collection.find().iterator();
-        List<Recipe> myRecipes = new ArrayList<>();
-        while (cur.hasNext()) {
-            myRecipes.add(cur.next());
+        try {
+            MongoCursor<Recipe> cur = collection.find().iterator();
+            List<Recipe> myRecipes = new ArrayList<>();
+            while (cur.hasNext()) {
+                myRecipes.add(cur.next());
+            }
+            LOGGER.info("Retrieved " + myRecipes.size() + " recipes");
+            return myRecipes;
+        } catch (MongoException me) {
+            LOGGER.severe("Failed to retrieve recipes: " + me.getMessage());
+            throw new RuntimeException("Database query failed", me);
         }
-        System.out.println("Retrieved " + myRecipes.size() + " recipes");
-        return myRecipes;
     }
 
     public int addRecipes(List<Recipe> recipes) {
         try {
             InsertManyResult result = collection.insertMany(recipes);
-            System.out.println("Inserted " + result.getInsertedIds().size() + " documents");
+            LOGGER.info("Inserted " + result.getInsertedIds().size() + " documents");
             return result.getInsertedIds().size();
         } catch (MongoException me) {
-            System.err.println("Unable to insert recipes: " + me);
+            LOGGER.severe("Unable to insert recipes: " + me.getMessage());
             return -1;
         }
     }
@@ -100,10 +136,10 @@ public class Persistence {
     public int deleteRecipes(Bson deleteFilter) {
         try {
             DeleteResult deleteResult = collection.deleteMany(deleteFilter);
-            System.out.printf("Deleted %d documents\n", deleteResult.getDeletedCount());
+            LOGGER.info("Deleted " + deleteResult.getDeletedCount() + " documents");
             return (int) deleteResult.getDeletedCount();
         } catch (MongoException me) {
-            System.err.println("Unable to delete recipes: " + me);
+            LOGGER.severe("Unable to delete recipes: " + me.getMessage());
             return -1;
         }
     }
@@ -114,93 +150,18 @@ public class Persistence {
     }
 
     public void main2() {
-        System.out.println("Initializing sample recipes...");
-        collection.deleteMany(Filters.exists("_id"));
-        addRecipes(recipes);
-        List<Recipe> myRecipes = getAllRecipes();
-        for (Recipe currentRecipe : myRecipes) {
-            System.out.printf("%s has %d ingredients and takes %d minutes to make\n",
-                    currentRecipe.getName(), currentRecipe.getIngredients().size(), currentRecipe.getPrepTimeInMinutes());
-        }
-        deleteRecipesByName(Arrays.asList("elotes", "fried rice"));
-    }
-
-    public static void main(String[] args) {
-        Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
-        ConnectionString mongoUri = new ConnectionString("mongodb+srv://ead2024:ead2024@ead-2023-24.lpclwdo.mongodb.net/");
-        String dbName = "ead_ca2";
-        String collectionName = "ead_2024";
-
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .codecRegistry(pojoCodecRegistry)
-                .applyConnectionString(mongoUri).build();
-
-        MongoClient mongoClient = null;
+        LOGGER.info("Initializing sample recipes...");
         try {
-            mongoClient = MongoClients.create(settings);
-        } catch (MongoException me) {
-            System.err.println("Unable to connect to the MongoDB instance due to an error: " + me);
-            System.exit(1);
-        }
-
-        MongoDatabase database = mongoClient.getDatabase(dbName);
-        MongoCollection<Recipe> collection = database.getCollection(collectionName, Recipe.class);
-
-        try {
-            InsertManyResult result = collection.insertMany(recipes);
-            System.out.println("Inserted " + result.getInsertedIds().size() + " documents.\n");
-        } catch (MongoException me) {
-            System.err.println("Unable to insert any recipes into MongoDB due to an error: " + me);
-            System.exit(1);
-        }
-
-        try (MongoCursor<Recipe> cursor = collection.find().iterator()) {
-            while (cursor.hasNext()) {
-                Recipe currentRecipe = cursor.next();
-                System.out.printf("%s has %d ingredients and takes %d minutes to make\n",
-                        currentRecipe.getName(), currentRecipe.getIngredients().size(), currentRecipe.getPrepTimeInMinutes());
+            collection.deleteMany(Filters.exists("_id"));
+            addRecipes(recipes);
+            List<Recipe> myRecipes = getAllRecipes();
+            for (Recipe currentRecipe : myRecipes) {
+                LOGGER.info(String.format("%s has %d ingredients and takes %d minutes to make",
+                        currentRecipe.getName(), currentRecipe.getIngredients().size(), currentRecipe.getPrepTimeInMinutes()));
             }
-        } catch (MongoException me) {
-            System.err.println("Unable to find any recipes in MongoDB due to an error: " + me);
+            deleteRecipesByName(Arrays.asList("elotes", "fried rice"));
+        } catch (Exception e) {
+            LOGGER.severe("Failed to initialize sample recipes: " + e.getMessage());
         }
-
-        Bson findPotato = Filters.eq("ingredients", "potato");
-        try {
-            Recipe firstPotato = collection.find(findPotato).first();
-            if (firstPotato == null) {
-                System.out.println("Couldn't find any recipes containing 'potato' as an ingredient in MongoDB.");
-                System.exit(1);
-            }
-        } catch (MongoException me) {
-            System.err.println("Unable to find a recipe to update in MongoDB due to an error: " + me);
-            System.exit(1);
-        }
-
-        Bson updateFilter = Updates.set("prepTimeInMinutes", 72);
-        FindOneAndUpdateOptions options = new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER);
-
-        try {
-            Recipe updatedDocument = collection.findOneAndUpdate(findPotato, updateFilter, options);
-            if (updatedDocument == null) {
-                System.out.println("Couldn't update the recipe. Did someone (or something) delete it?");
-            } else {
-                System.out.println("\nUpdated the recipe to: " + updatedDocument);
-            }
-        } catch (MongoException me) {
-            System.err.println("Unable to update any recipes due to an error: " + me);
-        }
-
-        Bson deleteFilter = Filters.in("name", Arrays.asList("elotes", "fried rice"));
-        try {
-            DeleteResult deleteResult = collection.deleteMany(deleteFilter);
-            System.out.printf("\nDeleted %d documents.\n", deleteResult.getDeletedCount());
-        } catch (MongoException me) {
-            System.err.println("Unable to delete any recipes due to an error: " + me);
-        }
-
-        mongoClient.close();
     }
 }
